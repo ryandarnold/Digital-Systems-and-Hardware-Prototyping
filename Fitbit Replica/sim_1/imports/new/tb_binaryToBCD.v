7c8915e0077feab6f4733d8e448c57c50ebe9958
Ryan Arnold <47573484+ryandarnold@users.noreply.github.com>
2026-05-20 15:11:06 -0700

    Refactor tb_binaryToBCD.v by removing comments
    Removed unnecessary comments and cleaned up the code.

--- Fitbit Replica/sim_1/imports/new/tb_binaryToBCD.v
+++ Fitbit Replica/sim_1/imports/new/tb_binaryToBCD.v
@@ -1,27 +1,11 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
 // Create Date: 09/17/2022 10:58:51 PM
-// Design Name: 
 // Module Name: tb_binaryToBCD
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module tb_binaryToBCD();
-
 
 
 reg [13:0] binaryInput = 0;
@@ -39,15 +23,8 @@ binaryToBCD uut (binaryInput, BCD_THOUSANDS, BCD_HUNDREDS, BCD_TENS, BCD_ONES);
 initial begin
 //insert input here
 
-//binaryInput = 14'b10011010100001; // 9889
-//binaryInput = 14'b10011100001111; //9999 WORKS
-
 binaryInput = 1;
 
-
 end
-
-
-
 
 endmodule
