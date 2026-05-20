c5dd7729ff18d67c509a23f2793a3ff0e5ebbbdb
Ryan Arnold <47573484+ryandarnold@users.noreply.github.com>
2026-05-20 15:52:15 -0700

    Refactor totalStepCount.v by cleaning comments
    Removed unnecessary comments and cleaned up code formatting.

--- Fitbit Replica/sources_1/imports/new/totalStepCount.v
+++ Fitbit Replica/sources_1/imports/new/totalStepCount.v
@@ -1,27 +1,11 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
 // Create Date: 09/18/2022 03:37:49 PM
-// Design Name: 
 // Module Name: totalStepCount
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module totalStepCount(
-    //input CLK, 
     input PULSE, input RESET,
     
     output [13:0] sevenSegOut, output [22:0] actualTotalStepsOut, output SI
@@ -29,7 +13,7 @@ module totalStepCount(
     
     //MAKE SURE TO SET BELOW COUNTERS TO ZERO
     reg [13:0] counter_SevenSegment = 0; // limited to output up to 9999
-    reg [22:0] counter_actualTotal = 0;  // 
+    reg [22:0] counter_actualTotal = 0;  
     reg SI_REG = 0;
     
     assign SI = SI_REG; 
@@ -48,7 +32,6 @@ module totalStepCount(
         
         else
         begin
-            //can I do a blocking statement THEN check it with an if statement to revert it back? 
             if (counter_actualTotal >= 9999) // need to keep incrementing counter_actualTotal and stop incrementing counter_SevenSegment
             begin
                 counter_actualTotal <= counter_actualTotal + 1;
@@ -63,18 +46,8 @@ module totalStepCount(
                 SI_REG <= 0;
             end
             
-            
-            
         end
     
-    
     end
-    
-    
-    
-    
-    
-    
-    
     
 endmodule
