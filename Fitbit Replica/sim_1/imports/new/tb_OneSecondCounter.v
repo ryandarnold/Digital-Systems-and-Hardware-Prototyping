1b6eb1b2ebd35e0a411f18674129d16ae8a667a0
Ryan Arnold <47573484+ryandarnold@users.noreply.github.com>
2026-05-20 15:07:53 -0700

    Clean up comments in tb_OneSecondCounter.v
    Removed unnecessary comments and metadata from tb_OneSecondCounter.v

--- Fitbit Replica/sim_1/imports/new/tb_OneSecondCounter.v
+++ Fitbit Replica/sim_1/imports/new/tb_OneSecondCounter.v
@@ -1,22 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
 // Create Date: 09/17/2022 03:37:33 PM
-// Design Name: 
 // Module Name: tb_OneSecondCounter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
