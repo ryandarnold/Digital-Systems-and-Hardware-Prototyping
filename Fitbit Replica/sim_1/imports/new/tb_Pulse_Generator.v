b25768f84f91128d04c317d9dd68182c4715204d
Ryan Arnold <47573484+ryandarnold@users.noreply.github.com>
2026-05-20 15:08:47 -0700

    Clean up tb_Pulse_Generator.v comments
    Removed unnecessary comments and cleaned up the code.

--- Fitbit Replica/sim_1/imports/new/tb_Pulse_Generator.v
+++ Fitbit Replica/sim_1/imports/new/tb_Pulse_Generator.v
@@ -1,22 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
 // Create Date: 09/16/2022 03:19:49 PM
-// Design Name: 
 // Module Name: tb_Pulse_Generator
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
@@ -42,15 +27,7 @@ MODE = 2'b11;
 RESET = 0;
 START = 1; 
 
-
-//#1500000000 // 1.5 billion = 1.5 seconds
-//RESET = 1;
-
-//#300000000 // 300,000,000 = 0.3 seconds later
-//RESET = 0;
-
 end
-
 
 
 endmodule
