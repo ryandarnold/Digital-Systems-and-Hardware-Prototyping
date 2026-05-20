be005ad457e1530332a6d293aa53c400a3117539
Ryan Arnold <47573484+ryandarnold@users.noreply.github.com>
2026-05-20 15:35:55 -0700

    Clean up header comments in combinationalMux_14BitOutput.v
    Removed unnecessary comments and metadata from the header.

--- Fitbit Replica/sources_1/imports/new/combinationalMux_14BitOutput.v
+++ Fitbit Replica/sources_1/imports/new/combinationalMux_14BitOutput.v
@@ -1,22 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
 // Create Date: 09/23/2022 05:44:09 PM
-// Design Name: 
 // Module Name: combinationalMux_14BitOutput
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
