42895d9e31ddc0bd749878d4a8b73d8233c6deb7
Ryan Arnold <47573484+ryandarnold@users.noreply.github.com>
2026-05-20 16:02:51 -0700

    Clean up binaryToHex module by removing comments
    Removed commented-out code and unnecessary notes.

--- MIPS CPU/sources_1/imports/new/binaryToHex.v
+++ MIPS CPU/sources_1/imports/new/binaryToHex.v
@@ -1,34 +1,13 @@
 `timescale 1ns / 1ps
 
-//NOTE: THIS MODULE WORKS PERFECTLY*****************
-
 module binaryToHexToSevenSeg(
     input [3:0] SingleHexRegisterValueToDisplay,
-    //input PerfectButtonLeft,
-    //input PerfectButtonRight,
-     
-    //output reg anode0,
     output reg [6:0] sevenSeg
-    /*output [3:0] hex8, 
-    output [3:0] hex7,
-    output [3:0] hex6,
-    output [3:0] hex5,
-    output [3:0] hex4,
-    output [3:0] hex3,
-    output [3:0] hex2,
-    output [3:0] hex1,
-    output [3:0] hex0*/
     );
-    
     
     
     always@(SingleHexRegisterValueToDisplay)
     begin
-        //anode0 = 0;
-    //anodeSelect0 = 0; // 0 means ON . 1 means OFF
-    //anodeSelect1 = 1;
-    //anodeSelect2 = 1;
-    //anodeSelect3 = 1;
         case (SingleHexRegisterValueToDisplay)
         4'b0000 : sevenSeg = 7'b1000000; // 64 for seven seg
         4'b0001 : sevenSeg = 7'b1111001; // 121 for seven seg
@@ -49,18 +28,6 @@ module binaryToHexToSevenSeg(
         default : sevenSeg = 7'b1111111; // 127 : and displays NOTHING
         
         endcase
-    
-    
     end
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
 endmodule
